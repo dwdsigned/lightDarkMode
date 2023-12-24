a6ba3460a1d2a5d6254cf8974f3b867292cb761a
Darius Williams <dariustwilliams668@gmail.com>
2023-12-24 18:50:02 -0500

    functionality complete

--- script.js
+++ script.js
@@ -91,3 +91,61 @@ function changeActive() {
 
 // Call the changeActive() inorder add/remove the 'active' class from section and anchor elements
   changeActive();
+
+
+
+
+
+
+
+  const toggleSwitch = document.querySelector(`input[type="checkbox"]`);
+  const nav = document.getElementById(`nav`);
+  const toggleIcon = document.getElementById(`toggle-icon`);
+  const textBox = document.getElementById(`text-box`);
+
+
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = `rgb(35 35 35 /50%)`;
+    textBox.style.backgroundColor = `rgb(235 235 235 / 50%)`;
+    toggleIcon.children[0].textContent = `Dark Mode`;
+    toggleIcon.children[1].classList.replace(`fa-sun`, `fa-moon`);
+}
+
+// Light Mode Style
+function lightMode() {
+    nav.style.backgroundColor = `rgb(235 235 235 / 50%)`;
+    textBox.style.backgroundColor = `rgb(35 35 35 /50%)`;
+    toggleIcon.children[0].textContent = `Light Mode`;
+    toggleIcon.children[1].classList.replace(`fa-moon`, `fa-sun`);
+}
+
+
+
+  // Switch the theme Dynamically
+  function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute(`data-theme`, `dark`);
+        localStorage.setItem(`theme`, `dark`);
+        darkMode();
+    } else {
+        document.documentElement.setAttribute(`data-theme`, `light`);
+        localStorage.setItem(`theme`, `light`);
+        lightMode();
+    }
+  }
+  // Light/Dark Mode Event Listener
+  toggleSwitch.addEventListener("change",switchTheme);
+
+  // Check Local Storage for Theme
+  const currentTheme = localStorage.getItem(`theme`);
+  if (currentTheme) {
+    document.documentElement.setAttribute(`data-theme`, currentTheme);
+  }
+
+// Checks Local Store state. If dark mode was enabled, switch the theme of the website to dark
+  if (currentTheme === `dark`) {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+
